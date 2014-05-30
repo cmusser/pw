eec26a297411367470b41311c17e8e44e2a6d6b4
Chuck Musser <cmusser@linktv.org>
2014-05-30 12:39:39 -0700

    Move common load/store password functions into module.

---
+++ pwutils.py
@@ -0,0 +1,25 @@
+import json
+import nacl.secret
+import nacl.utils
+import scrypt
+
+
+def load(pw_filename, password):
+    with open(pw_filename, "r") as pw_file:
+        salt = pw_file.read(64)
+        key = scrypt.hash(password, salt, buflen=32)
+        box = nacl.secret.SecretBox(key)
+        json_data = box.decrypt(pw_file.read())
+
+    return json.loads(json_data)
+
+
+def save(pw_filename, password, pw_data):
+    with open(pw_filename, "w") as pw_file:
+        salt = nacl.utils.random(64)
+        key = scrypt.hash(password, salt, buflen=32)
+        box = nacl.secret.SecretBox(key)
+        nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+        encrypted = box.encrypt(json.dumps(pw_data), nonce)
+        pw_file.write(salt)
+        pw_file.write(encrypted)
