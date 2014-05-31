686139d3dbe765282d1d116b22fe512870f56bf1
Chuck Musser <cmusser@linktv.org>
2014-05-30 19:48:49 -0700

    Make installable package (via setuptools).

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pw',
+    version='0.1.0',
+    author='Chuck Musser',
+    author_email='cmusser@sonic.net',
+    packages=['pw'],
+    scripts=['bin/pw','bin/buildpw'],
+    url='http://pypi.python.org/pypi/Pw/',
+    license='LICENSE.txt',
+    description='Simple, secure command-line password manager.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pynacl",
+        "scrypt",
+    ],
+)
