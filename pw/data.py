c0d3863672f98c15c65529ead4a7400f9b80d3da
Chuck Musser <cmusser@linktv.org>
2014-06-09 12:12:38 -0700

    extra blank line, per PEP8

--- pw/data.py
+++ pw/data.py
@@ -4,6 +4,7 @@ import re
 fields = 'site', 'username', 'password', 'extra'
 max_field_len = len(max(fields, key=len))
 
+
 def find(search_term, pw_data):
     return [name for name in pw_data if re.search(search_term, name,
                                                   re.I)]
