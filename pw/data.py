d7e9e57ac67b9f7aea9710c38ed74c4b0a188443
Chuck Musser <cmusser@sonic.net>
2014-07-01 21:12:09 -0700

    Refactor: move editing credential lookup into utility library

--- pw/data.py
+++ pw/data.py
@@ -1,3 +1,4 @@
+import pw.util
 import re
 
 
@@ -8,3 +9,20 @@ max_field_len = len(max(fields, key=len))
 def find(search_term, pw_data):
     return [name for name in pw_data if re.search(search_term, name,
                                                   re.I)]
+
+
+def lookup_credential_for_edit(pw_data, search_term):
+    names = find(search_term, pw_data)
+    match_count = len(names)
+    if match_count == 0:
+        credential_name = search_term
+    elif match_count == 1:
+        credential_name = names[0]
+    else:
+        n = 0
+        for name in names:
+            n = 1
+            print "\n{}.) {}".format(n, name)
+        credential_name = names[pw.util.prompt_for_idx(names)]
+
+    return credential_name
