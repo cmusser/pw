ca19c93e741f3eddc94101a943ac97e36fe63f97
Chuck Musser <cmusser@linktv.org>
2014-11-20 23:52:27 -0800

    More specific objects; more common functionality.
    - pw.core now has separate Cli and Store classes
      for handling the CLI functionality and datastore
      functionality separately.
    
    - Cli instances have an instance of Store and a
      new run() method, which DRYs up the similar
      functionality that appeared in all the interactive
      programs. Finally, there is a new static method
      that provides calling applications with the
      basic CLI related options in an initialized
      argparse parser.
    
    - Store objects now own the field-related
      properties, as well as the load(), save() and
      find() methods.
    
    - update the rmpw, pw, mv and editpw programs to
      take advantage of the new functionality--base
      arguments, interactive loop or terminate flow--
      and properly use the new Cli object in their
      process_func closures. Cli instances now have a
      pw_store property, which contains the password
      storage object instance.
    
    - update the buildpw, chow, and dumppw program to
      use the Store object, because they do not need
      the interactive functionality provided by the
      Cli object.

--- pw/core.py
+++ pw/core.py
@@ -1,3 +1,5 @@
+import argparse
+import getpass
 import json
 import nacl.secret
 import nacl.utils
@@ -5,11 +7,54 @@ import os
 import re
 import readline
 import scrypt
+import sys
+
+
+class Store:
+
+    fields = 'site', 'username', 'password', 'extra'
+    max_field_len = len(max(fields, key=len))
+
+    def __init__(self, pw_name, password):
+        self.pw_filename = os.path.expanduser('~/.pw/' + pw_name + '.pw')
+        self.password = password
+        self.data = None
+
+    def load(self):
+        with open(self.pw_filename, "r") as pw_file:
+            salt = pw_file.read(64)
+            key = scrypt.hash(self.password, salt, buflen=32)
+            box = nacl.secret.SecretBox(key)
+            json_data = box.decrypt(pw_file.read())
+
+        self.data = json.loads(json_data)
+
+    def save(self):
+        with open(self.pw_filename, "w") as pw_file:
+            salt = nacl.utils.random(64)
+            key = scrypt.hash(self.password, salt, buflen=32)
+            box = nacl.secret.SecretBox(key)
+            nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
+            encrypted = box.encrypt(json.dumps(self.data), nonce)
+            pw_file.write(salt)
+            pw_file.write(encrypted)
+
+    def find(self, search_term):
+        return [name for name in self.data if re.search(search_term, name,
+                                                        re.I)]
 
 
 class Cli:
-    fields = 'site', 'username', 'password', 'extra'
-    max_field_len = len(max(fields, key=len))
+
+    @staticmethod
+    def create_arg_parser(description):
+        parser = argparse.ArgumentParser(description=description)
+        parser.add_argument('pw_file', help='password list')
+        parser.add_argument('credential_name', nargs='?', default=None,
+                            help=('name of credential (if not specified, '
+                                  'command will prompt.'))
+
+        return parser
 
     @staticmethod
     def prompt_for_idx(list):
@@ -31,21 +76,15 @@ class Cli:
 
         return choice_idx
 
-    @staticmethod
-    def pw_name_to_path(pw_name):
-        return os.path.expanduser('~/.pw/' + pw_name + '.pw')
-
-    def __init__(self, pw_name, password):
-        self.pw_filename = self.pw_name_to_path(pw_name)
-        self.password = password
-        self.pw_data = None
-
-    def find(self, search_term):
-        return [name for name in self.pw_data if re.search(search_term, name,
-                                                           re.I)]
+    def __init__(self, pw_name):
+        try:
+            self.pw_store = Store(pw_name, getpass.getpass())
+        except (KeyboardInterrupt):
+            print
+            sys.exit()
 
     def lookup_credential_for_edit(self, search_term):
-        names = self.find(search_term)
+        names = self.pw_store.find(search_term)
         match_count = len(names)
         if match_count == 0:
             credential_name = search_term
@@ -60,31 +99,29 @@ class Cli:
 
         return credential_name
 
-    def load(self):
-        with open(self.pw_filename, "r") as pw_file:
-            salt = pw_file.read(64)
-            key = scrypt.hash(self.password, salt, buflen=32)
-            box = nacl.secret.SecretBox(key)
-            json_data = box.decrypt(pw_file.read())
-
-        self.pw_data = json.loads(json_data)
-
-    def save(self):
-        with open(self.pw_filename, "w") as pw_file:
-            salt = nacl.utils.random(64)
-            key = scrypt.hash(self.password, salt, buflen=32)
-            box = nacl.secret.SecretBox(key)
-            nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-            encrypted = box.encrypt(json.dumps(self.pw_data), nonce)
-            pw_file.write(salt)
-            pw_file.write(encrypted)
-
     def prompt_loop(self, prompt, input_function):
         try:
             while True:
                 line = raw_input('{}>'.format(prompt))
                 if line and not line.isspace():
-                    input_function(self, line)
+                    input_function(self.pw_store, line)
                     print
         except EOFError:
             pass
+
+    def run(self, prompt_str, process_func, credential_name=None):
+        try:
+            if credential_name is None:
+                # The result of the load is not used here, but it gives the
+                # function a chance to throw an exception if the file cannot
+                # be accessed or the password was invalid..
+                self.pw_store.load()
+                self.prompt_loop(prompt_str, process_func)
+            else:
+                process_func(self, credential_name)
+
+        except (IOError, nacl.exceptions.CryptoError) as e:
+            print e
+
+        except (KeyboardInterrupt, EOFError):
+            print
