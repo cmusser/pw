bf510daf3a95346d8027ade72ce9bd41d2b8b432
Chuck Musser <cmusser@sonic.net>
2014-12-30 14:45:53 -0800

    Database design improvements
    The database module is now split into a base class with common methods
    and properties and a File class that make use of it. This cleans up
    File, and paves the way for other storage options.

--- pw/db.py
+++ pw/db.py
@@ -16,60 +16,65 @@ MAX_FIELD_LEN = len(max(FIELDS, key=len))
 
 class Base(object):
 
-    def encrypt(password, data):
-        salt = nacl.utils.random(64)
+    def __init__(self):
+        '''
+        Initialize object. Note that the only assumptions are that the data
+        is a dictionary and that the only functions needed are the ones
+        defined below. The way to store the data and decisions such as
+        remembering a password devolve to subclasses.
+        '''
+        self.data = {}
+
+    def export_encrypted(self, salt, password):
+        '''
+        Return the data dictionary as an encrypted JSON text. This format
+        is suitable for storage or transmission.
+        '''
         key = scrypt.hash(password, salt, buflen=32)
         box = nacl.secret.SecretBox(key)
         nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-        return box.encrypt(json.dumps(data), nonce)
+        return box.encrypt(json.dumps(self.data), nonce)
 
-    def decrypt(salt, password, encrypted):
+    def import_from_encrypted(self, salt, password, encrypted):
+        '''
+        Load the data dictionary from an encrypted JSON text.
+        '''
         key = scrypt.hash(password, salt, buflen=32)
         box = nacl.secret.SecretBox(key)
-        return json.loads(box.decrypt(encrypted))
+        self.data = json.loads(box.decrypt(encrypted))
 
 
-class File(object):
+class File(Base):
 
     def __init__(self, pw_name, pw_func, access=RW):
         self.pw_filename = os.path.expanduser('~/.pw/' + pw_name + '.pw')
         self.pw_func = pw_func
+        super(File, self).__init__()
 
         try:
             with open(self.pw_filename,
                       "r" if access == RO else 'r+') as pw_file:
                 self.password = self.pw_func()
-                salt = pw_file.read(64)
-                key = scrypt.hash(self.password, salt, buflen=32)
-                box = nacl.secret.SecretBox(key)
-                self.data = json.loads(box.decrypt(pw_file.read()))
+                self.import_from_encrypted(pw_file.read(64), self.password,
+                                           pw_file.read())
         except IOError as e:
             if e.errno == errno.ENOENT and access == RW_CREATE_EMPTY:
                 self.password = self.pw_func('Password for new database '
                                              '"{}": '.format(pw_name))
-                self.data = {}
                 self.save()
             else:
                 raise e
 
     def load(self):
         with open(self.pw_filename, "r") as pw_file:
-            salt = pw_file.read(64)
-            key = scrypt.hash(self.password, salt, buflen=32)
-            box = nacl.secret.SecretBox(key)
-            json_data = box.decrypt(pw_file.read())
-
-        self.data = json.loads(json_data)
+            self.import_from_encrypted(pw_file.read(64), self.password,
+                                       pw_file.read())
 
     def save(self):
         with open(self.pw_filename, "w") as pw_file:
             salt = nacl.utils.random(64)
-            key = scrypt.hash(self.password, salt, buflen=32)
-            box = nacl.secret.SecretBox(key)
-            nonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-            encrypted = box.encrypt(json.dumps(self.data), nonce)
             pw_file.write(salt)
-            pw_file.write(encrypted)
+            pw_file.write(self.export_encrypted(salt, self.password))
 
     def find(self, search_term):
         return [name for name in self.data if re.search(search_term, name,
